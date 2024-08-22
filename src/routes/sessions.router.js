a5b75b2d137da27afaa5c3448f39717b9175ab1b
cguirin <carlaguirin07@gmail.com>
2024-08-22 13:53:41 -0300

    cambios

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -66,8 +66,8 @@ router.post("/login", async (req, res) => {
 
         // Configuramos la cookie con el token
         res.cookie("coderCookieToken", token, {
-            maxAge: 3600000, // 1 hora
-            httpOnly: true   // Solo accesible por HTTP
+            maxAge: 3600000, 
+            httpOnly: true   
         });
 
         res.redirect("/api/sessions/current");
